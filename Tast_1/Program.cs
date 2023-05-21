e0777d8a12af41032cda024999af9245100fb72f
Mikhail <79122806999@ya.ru>
2023-05-21 14:38:25 +0500

    Tasks

--- Tast_1/Program.cs
+++ Tast_1/Program.cs
@@ -9,17 +9,20 @@ int Max(int arg1, int arg2, int arg3)
 }
 int a1 = 15;
 int a2 = 1647;
-int a3 = 152411;
+int a3 = 15240011;
 int a4 = 15774;
-int a5 = 15424147;
+int a5 = 1542414007;
 int a6 = 15246;
 int a7 = 1524;
 int a8 = 15454;
 int a9 = 155;
 
-int max1 = Max(a1, a2, a3);
-int max2 = Max(a4, a5, a6);
-int max3 = Max(a7, a8, a9);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(a4, a5, a6);
+// int max3 = Max(a7, a8, a9);
+int max = Max(
+    Max(a1, a2, a3), 
+    Max(a4, a5, a6), 
+    Max(a7, a8, a9));
 
 System.Console.WriteLine(max);
