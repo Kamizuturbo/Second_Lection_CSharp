4e98c7c6f4a79e45fc3f121cd93dc819c8b65a93
Mikhail <79122806999@ya.ru>
2023-05-21 14:47:00 +0500

    Tasks

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ищем максимальное из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+int[] array ={1,2,3,4545,454,5656,545,565,6};
+
+int result = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])
+    );
+
+System.Console.WriteLine(result);
